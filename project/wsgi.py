0c23bfd95ec08a18d5aa66ee0ac92bd17085fbbc
UdiAdi <udit.aditya94@gmail.com>
2016-08-09 23:28:02 +0530

    wsgi ch'e'nged

--- project/wsgi.py
+++ project/wsgi.py
@@ -7,15 +7,21 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
+
+# +++++++++++ DJANGO +++++++++++
+# To use your own django app use code like this:
 import os
 import sys
 
-## assuming your Django settings file is at '/home/my_username/projects/my_project/settings.py'
-path = '/home/udit/Desktop/djangoProject/venv/project/project'
+# assuming your django settings file is at '/home/UdiAdi/myblog/project/settings.py'
+path = '/home/UdiAdi/myblog'
 if path not in sys.path:
     sys.path.append(path)
 
-os.environ['DJANGO_SETTINGS_MODULE'] = 'my_project.settings'
+os.environ['DJANGO_SETTINGS_MODULE'] = 'myblog.project.settings'
 
+## Uncomment the lines below depending on your Django version
+###### then, for django >=1.5:
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
+
